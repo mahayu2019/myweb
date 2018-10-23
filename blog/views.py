a7e6436d393b9486baa391614b00d418f69a64e7
mahayu2019 <2353987275@qq.com>
2018-10-23 20:21:51 +0800

    还原

--- blog/views.py
+++ blog/views.py
@@ -86,4 +86,4 @@ def blog_with_date(request, year, month):
     context = get_blog_list_common_data(request, blog_all_list)
     context['blog_with_date'] = '%s年%s月' % (year, month)
     
-    return render_to_response('blog_with_date.html', context)
+    #return render_to_response('blog_with_date.html', context)
