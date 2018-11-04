2d9d52c34567639866431831584047d32d96ed8b
mahayu2019 <2353987275@qq.com>
2018-11-04 15:22:52 +0800

    发布评论处理方法完善,前端美化

--- comment/views.py
+++ comment/views.py
@@ -4,19 +4,31 @@ from django.urls import reverse
 from django.contrib.contenttypes.models import ContentType
 
 
+# 提交评论
 def update_comment(request):
-    user = request.user
-    text = request.POST.get('text', '')
-    content_type = request.POST.get('content_type', '')
-    object_id = int(request.POST.get('object_id', ''))
-    model_class = ContentType.objects.get(model=content_type).model_class()
-    model_obj = model_class.objects.get(pk=object_id)
+    referer = request.META.get('HTTP_REFERER', reverse('home'))  # 回退到上一次的页面
 
-    comment = Comment()
-    comment.user = user
-    comment.text = text
-    comment.content_object = model_obj
-    comment.save()
+    # 数据检查
+    if not request.user.is_authenticated:
+        # 错误之后的返回按钮回退指定页面
+        return render(request, 'error.html', {'message': '请登录!', 'redirect_to': referer})
 
-    referer = request.META.get('HTTP_REFERER', reverse('home'))
-    return redirect(referer)
+    text = request.POST.get('text', '').strip()
+    if text == '':
+        return render(request, 'error.html', {'message': '评论不能为空!', 'redirect_to': referer})
+
+    try:
+        content_type = request.POST.get('content_type', '')
+        object_id = int(request.POST.get('object_id', ''))
+        model_class = ContentType.objects.get(model=content_type).model_class()
+        model_obj = model_class.objects.get(pk=object_id)
+    except Exception as e:
+        return render(request, 'error.html', {'message': '评论对象不存在!', 'redirect_to': referer})
+
+        comment = Comment()
+        comment.user = user
+        comment.text = text
+        comment.content_object = model_obj
+        comment.save()
+
+        return redirect(referer)
